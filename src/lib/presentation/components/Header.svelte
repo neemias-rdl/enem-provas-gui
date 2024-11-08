6de07b7e879780d0a54e9c2f2623f0cfb9ae4b74
neemias-rdl <neemias2005@gmail.com>
2024-11-08 19:22:01 -0300

    added header component

---
+++ src/lib/presentation/components/Header.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+</script>
+
+<main>
+  <header
+    class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom text-background-dark text-bg-dark"
+  >
+    <ul class="nav nav-pills">
+      <li class="nav-item">
+        <a href="/" class="nav-link link-light">Home</a>
+      </li>
+      <li class="nav-item">
+        <a href="/provas" class="nav-link link-light">Provas</a>
+      </li>
+      <li class="nav-item">
+        <a href="/" class="nav-link link-light">Questionario</a>
+      </li>
+    </ul>
+  </header>
+</main>
